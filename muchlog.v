43c81fe06877c1eebdd4596ef271f9d8294a8e4f
João Francisco Olivetti <jiotich@gmail.com>
2021-04-27 22:56:10 -0300

    Update muchlog.v

--- muchlog.v
+++ muchlog.v
@@ -1,75 +1,83 @@
 module maosma(clk, in, out);
 
-integer given;
+	integer given;
 
-input clk;
-input [1:0] in;
-output [1:0] out;
+	input clk;
+	input [1:0] in;
+	output [1:0] out;
 
-reg [1:0] out;
-reg [1:0] state;
+	reg [1:0] out;
+	reg [1:0] state;
 
-parameter zero=0, um=1, dois=2, tres=3;
-/*
-zero = insuficiente
-um = processa o troco (caso seja cancelado sem terminar compra)
-dois = dá o doce quando não é necessário troco
-tres = dá o doce e o troco
-*/
+	parameter zero=0, um=1, dois=2, tres=3;
+	/*
+	zero = aguardando total ser 30 (ou seja, estado de recepção de valores)
+	um = processa o troco (caso seja cancelado sem terminar compra)
+	dois = dá o doce quando não é necessário troco
+	tres = dá o doce e o troco
+	*/
 
-initial begin
-	given = 0;
-	state = zero;
-end
+	initial begin
+		given = 0;
+		state = zero;
+	end
 
-always @(state) 
-     begin
-          case (state)
-                zero:
-                    out = 2'b00;
-                um:
-                    out = 2'b01;
-                dois:
-                    out = 2'b10;
-                tres:
-                    out = 2'b11;
-                default:
-                    out = 2'b00;
-          endcase
-     end
+	always @(state) 
+		 begin
+			  case (state)
+					zero:
+						out = 2'b00;
+					um:
+						begin
+							out = 2'b01;
+						end
+					dois:
+						out = 2'b10;
+					tres:
+						out = 2'b11;
+					default:
+						out = 2'b00;
+			  endcase
+		 end
 
-always @(posedge clk)
-    begin
-	$display("\n +Dado: ", given);
-		if (given >= 30)
-			begin
-				$display(" Troco: %b", given-30);
+	always @(posedge clk)
+		begin
+			if (given >= 30)
+				begin
+					if (given == 30)
+						begin
+							state = dois;
+							$display("TOMA DOCE");
+						end
+					else
+						$display(">TOMA DOCE e TROCO: %d", given-30);
+					given = 0;
+				end
+
+			else if (in == 2'b00)
 				given = 0;
-			end
-
-		else if (in == 2'b00)
-			given = 0;
-		else if (in == 2'b01)
-			given+=5;
-		else if (in == 2'b10)
-			given+=10;
-		else if (in == 2'b11)
-			given+=25;
-	
-	    case (state)
-            zero:
-                state = um;
-            um:
-				if (in)
-                     state = zero;
-                else
-                    state = dois;
-            dois:
-                state = tres;
-            tres:
-                state = zero;
-            endcase
-     end
+			else if (in == 2'b01)
+				given+=5;
+			else if (in == 2'b10)
+				given+=10;
+			else if (in == 2'b11)
+				given+=25;
+			$display("\n>Total Dado: ", given);
+			case (state)
+				zero:
+					if (in)
+					state = um;
+				um:
+					if (in)
+						 state = zero;
+					else
+						state = dois;
+				dois:
+					state = zero;
+				tres:
+					state = zero;
+				endcase
+		 end
 
 endmodule
 
@@ -83,25 +91,15 @@ module testbench();
 
     initial begin
 
-        //$monitor("IN = %b", out);
+        $monitor("IN = %b, clk = %b", in, clk);
 
-        clk = 1'b0;
+        clk = 1'b1;
 		in = 2'b10;
 		#2;
 		in = 2'b10;
 		#2;
 		in = 2'b11;
 		#2;
-		in = 2'b10;
-		#2;
-		in = 2'b11;
-		#2;
-		in = 2'b11;
-		#2;
-		in = 2'b10;
-		#2;
-		in = 2'b11;
-		
         $finish;
     
     end
